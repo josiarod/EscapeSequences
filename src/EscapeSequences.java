5e13b11ca20329f6752c1e4915f9adba0f77b7ab
josiarod <josiarodriguez@gmail.com>
2020-01-23 17:05:59 -0500

    escape sequences

---
+++ src/EscapeSequences.java
@@ -0,0 +1,8 @@
+public class EscapeSequences {
+    public static void main(String[] args){
+        System.out.println("********\n*\t\t*\n*\t\t*\n*\t\t*\n*\t\t*\n********\n\n");
+        System.out.println("  ***\n *\t *\n*\t  * \n*\t  *\n *\t *\n  ***\n\n");
+        System.out.println("   *\n *\t *\n*\t  *\n *\t *\n   * ");
+
+    }
+}
